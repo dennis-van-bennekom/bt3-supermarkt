5000b25709e32b7065a26819c7ffa2b1d5fe1dd4
Dennis van Bennekom <vanbennekomdennis@gmail.com>
2016-03-24 15:25:32 +0100

    viewport

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ function total() {
   <meta charset="UTF-8">
   <title>Supermarkt</title>
   <link rel="stylesheet" href="css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
   <div class="wrapper">
